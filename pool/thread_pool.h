eb01c43925c195a902a35264203ae93c6bb6cf44
lijiang99 <bd7xzz@163.com>
2023-08-01 18:49:40 +0800

    修改生产者/消费者模型

--- pool/thread_pool.h
+++ pool/thread_pool.h
@@ -37,31 +37,19 @@ class thread_pool {
 		task_queue_type* _task_queue;
 		// 保护请求队列的互斥锁
 		std::mutex _mutex;
-		// 条件变量判断是否有任务需要处理
-		// 需要搭配互斥锁一起使用
-		std::condition_variable _cond;
+		// 用于生产者/消费者（工作线程）模型的条件变量，需要搭配互斥锁一起使用
+		std::condition_variable _cond_producer;
+		std::condition_variable _cond_consumer;
 
 	public:
 		// 构造函数
 		thread_pool(std::size_t thread_num, std::size_t max_requests);
 
-		// 析构函数，关闭并释放线程池
-		~thread_pool() {
-#ifndef NDEBUG
-			std::cout << "\ndestroy thread pool..." << std::endl;
-#endif
-			_pool_status = _pool_shutdown; delete _task_queue;
-			// 唤醒所有阻塞的工作线程，由于此时线程池变成了关闭状态
-			// 那么worker中的循环判断就不成立，因此工作线程退出
-			_cond.notify_all();
-#ifndef NDEBUG
-			std::cout << "** " << (_pool_status ? "(startup)" : "(shutdown)")
-				<< " thread pool size => 0" << std::endl;
-#endif
-		}
+		// 析构函数
+		~thread_pool();
 
 		// 向请求队列中添加任务
-		bool add_task(task_type&& task);
+		void add_task(task_type&& task);
 
 		// 工作线程从请求队列中拉取任务进行处理
 		void worker();
@@ -93,22 +81,41 @@ thread_pool<Callback>::thread_pool(std::size_t thread_num, std::size_t max_reque
 #endif
 }
 
+// 析构函数，关闭并释放线程池
+template <typename Callback>
+thread_pool<Callback>::~thread_pool() {
+#ifndef NDEBUG
+	std::cout << "\ndestroy thread pool..." << std::endl;
+#endif
+	_pool_status = _pool_shutdown; delete _task_queue;
+	_cond_producer.notify_all();
+	// 唤醒所有阻塞的工作线程，由于此时线程池变成了关闭状态
+	// 那么worker中的循环判断就不成立，因此工作线程退出
+	_cond_consumer.notify_all();
+#ifndef NDEBUG
+	std::cout << "** " << (_pool_status ? "(startup)" : "(shutdown)")
+		<< " thread pool size => 0" << std::endl;
+#endif
+}
+
 // 向请求队列中添加任务，右值引用用于模板转发实参
 // 函数形参为指向模板类型参数的右值引用，可保持实参的所有类型信息
 template <typename Callback>
-bool thread_pool<Callback>::add_task(task_type&& task) {
+void thread_pool<Callback>::add_task(task_type&& task) {
 	// 向请求队列中添加任务，需要对请求队列进行锁保护
 	std::unique_lock<std::mutex> lock(_mutex);
 #ifndef NDEBUG
 	std::cout << "\nadd request into task queue..." << std::endl;
 #endif
-	// 若请求队列中的任务数超出最大请求数上限，则拒绝请求
-	if (_task_queue->size() == _max_requests) {
+	// 若请求队列中的任务数超出最大请求数上限，应该延迟添加，而不应该拒绝请求
+	// 即阻塞生产者线程，等待工作线程取出任务为请求队列腾出空间以添加新任务
+	while (_task_queue->size() >= _max_requests) {
 #ifndef NDEBUG
-		std::cout << "** (failure) task queue size => " << _task_queue->size() << std::endl;
+		std::cout << "** (wait) task queue size => " << _task_queue->size() << std::endl;
 #endif
-		lock.unlock();
-		return false;
+		// 若生产者线程此时处于加锁状态，则自动解锁，但不解除阻塞
+		// 若生产者线程解除阻塞，则再次加锁，以执行临界区（while循环后面的代码）
+		_cond_producer.wait(lock);
 	}
 	// 向请求队列中添加新任务，forward用于保持实参类型信息
 	// emplace在队列尾部调用task的构造函数来添加一个任务，效率更高
@@ -120,12 +127,11 @@ bool thread_pool<Callback>::add_task(task_type&& task) {
 	// 2. 未自定义拷贝操作、析构且对象所有成员可移动，则编译器默认合成
 	_task_queue->emplace(std::forward<task_type>(task));
 #ifndef NDEBUG
-	std::cout << "** (success) task queue size => " << _task_queue->size() << std::endl;
+	std::cout << "** (done) task queue size => " << _task_queue->size() << std::endl;
 #endif
 	// 解锁并唤醒阻塞在条件变量上的一个工作线程，让其处理任务
 	lock.unlock();
-	_cond.notify_one();
-	return true;
+	_cond_consumer.notify_one();
 }
 
 // 工作线程从请求队列中拉取任务进行处理
@@ -151,8 +157,9 @@ void thread_pool<Callback>::worker() {
 			std::cout << "\nprocess task => " << &task << "..." << std::endl;
 #endif
 			_task_queue->pop();
-			// 取出任务就解锁，以降低锁粒度
+			// 取出任务就解锁，并唤醒可能阻塞的生产者线程
 			lock.unlock();
+			_cond_producer.notify_one();
 			// 工作线程执行任务
 			task();
 #ifndef NDEBUG
@@ -163,15 +170,15 @@ void thread_pool<Callback>::worker() {
 			lock.lock();
 		}
 		// 无任务则工作线程阻塞以待有任务处理
-		// 若此时处于加锁状态，则自动解锁，但不解除阻塞
-		// 若线程解除阻塞，竞争到了任务，则再次加锁，以执行临界区任务（下一次循环）
 		else {
 #ifndef NDEBUG
 			std::cout << "\ntask queue is empty..." << std::endl;
 			std::cout << "** (wait) worker thread => " << std::hex
 				<< std::this_thread::get_id() << std::noshowbase << std::endl;
 #endif
-			_cond.wait(lock);
+			// 若工作线程此时处于加锁状态，则自动解锁，但不解除阻塞
+			// 若工作线程解除阻塞，则再次加锁，以执行临界区（下一次循环）
+			_cond_consumer.wait(lock);
 		}
 	}
 }
