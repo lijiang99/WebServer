ca74b5e5919f251c2888cac86b547886d4fd27d4
lijiang99 <bd7xzz@163.com>
2023-09-21 15:19:15 +0800

    可删除堆

--- timer/test_timer.cpp
+++ timer/test_timer.cpp
@@ -10,18 +10,20 @@ void hello(client_data *data) {
 int main() {
 	timer_heap<util_timer> heap;
 	util_timer *timer = nullptr;
-	/* timer.timeout_callback = hello; */
+	util_timer *random_timer = nullptr;
 
 	for (int i = 0; i < 5; ++i) {
 		timer = new util_timer();
+		if (i == 2) random_timer = timer;
 		std::chrono::milliseconds interval = (i & 1 ? std::chrono::milliseconds(100) : std::chrono::milliseconds(500));
-		timer->expire = std::chrono::high_resolution_clock::now() + interval;
+		timer->expire = std::chrono::high_resolution_clock  ::now() + interval;
 		timer->timeout_callback = hello;
 		heap.push_timer(timer);
 		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
 
-	/* for (int i = 0; i < 2; ++i) { */
+	heap.del_timer(random_timer);
+	random_timer = nullptr;
 	for (int i = 0; i < 5; ++i) {
 		heap.tick();
 		std::this_thread::sleep_for(std::chrono::milliseconds(300));
--- timer/timer.h
+++ timer/timer.h
@@ -44,33 +44,43 @@ struct util_timer {
 	expire_type expire; // 定时器的超时时间（绝对时间）
 	callback_type timeout_callback; // 定时器的回调函数
 	client_data* user_data; // 用户数据
+	std::size_t id; // 记录定时器在堆中的索引位置，用于快速定位
 };
 
 // 模板函数，比较两个指针所指向的定时器的超时时间，作为时间堆中的比较准则
-template <typename Timer>
-struct greater_exprie {
-	bool operator()(const Timer* lhs, const Timer* rhs) { return lhs->expire > rhs->expire; }
-};
+/* template <typename Timer> */
+/* struct greater_exprie { */
+/* 	bool operator()(const Timer* lhs, const Timer* rhs) { return lhs->expire > rhs->expire; } */
+/* }; */
 
 // 时间堆类（应以最小堆实现）
-template <typename Timer, typename Compare = greater_exprie<Timer>>
+/* template <typename Timer, typename Compare = greater_exprie<Timer>> */
+template <typename Timer>
 class timer_heap {
 	private:
 		typedef std::vector<Timer*> heap_type;
 		/* typedef bool(*compare_type)(const Timer*, const Timer*); */
 
-		heap_type *_heap; // 以vector作为时间堆的底层容器
-		Compare _compare; // 用于调整堆结构的比较准则
+		heap_type _heap; // 以vector作为时间堆的底层容器
+		/* Compare _compare; // 用于调整堆结构的比较准则 */
+	
+	private:
+		// 交换堆中的两个元素时需要交换定时器的底层id
+		void swap(Timer* &lhs, Timer* &rhs) { std::swap(lhs->id, rhs->id); std::swap(lhs, rhs); }
+		// 上滤和下滤操作
+		bool shift_up(std::size_t hole_idx);
+		bool shift_down(std::size_t hole_idx);
 
 	public:
 		// 构造函数，初始化一个空堆
-		timer_heap(Compare comp = Compare()) : _compare(comp) {
+		/* timer_heap(Compare comp = Compare()) : _compare(comp) { */
+		timer_heap() {
 #ifndef NDEBUG
 			std::cout << "\ninitialize timer heap..." << std::endl;
 #endif
-			_heap = new heap_type();
+			/* _heap = new heap_type(); */
 #ifndef NDEBUG
-			std::cout << "** heap size => " << _heap->size() << std::endl;
+			std::cout << "** heap size => " << _heap.size() << std::endl;
 #endif
 		}
 		// 析构函数，销毁时间堆
@@ -78,65 +88,129 @@ class timer_heap {
 #ifndef NDEBUG
 			std::cout << "\ndestroy timer heap..." << std::endl;
 #endif
-			for (typename heap_type::size_type i = 0; i < _heap->size(); ++i) {
+			/* for (typename heap_type::size_type i = 0; i < _heap->size(); ++i) { */
+			for (typename heap_type::reverse_iterator iter = _heap.rbegin(); iter < _heap.rend(); ++iter) {
 #ifndef NDEBUG
-				std::cout << "** deallocate timer expire => " << get_format_time(_heap->at(i)->expire) << std::endl;
+				std::cout << "** deallocate timer expire => " << get_format_time((*iter)->expire) << std::endl;
 #endif
-				delete (*_heap)[i];
+				delete *iter;
 			}
-			delete _heap;
+			/* delete _heap; */
 #ifndef NDEBUG
 			std::cout << "** deallocate timer heap" << std::endl;
 #endif
 		}
 
 		// 向堆中添加一个定时器
-		inline void push_timer(Timer *timer);
+		void push_timer(Timer *timer);
 		// 删除堆顶的定时器
-		inline void pop_timer();
+		void pop_timer();
+		// 删除堆中任意位置的定时器
+		void del_timer(Timer *timer);
 
 		// 心搏函数
 		void tick();
 };
 
+// 在堆中执行上滤操作
+template <typename Timer>
+bool timer_heap<Timer>::shift_up(std::size_t hole_idx) {
+	std::size_t tmp_idx = hole_idx;
+	std::size_t parent_idx = (hole_idx - 1) >> 1;
+	while (hole_idx > 0) {
+		if (_heap[parent_idx]->expire < _heap[hole_idx]->expire) break;
+		swap(_heap[parent_idx], _heap[hole_idx]);
+		hole_idx = parent_idx;
+		parent_idx = (hole_idx - 1) >> 1;
+	}
+	return tmp_idx ^ hole_idx;
+}
+
+// 在堆中执行下滤操作
+template <typename Timer>
+bool timer_heap<Timer>::shift_down(std::size_t hole_idx) {
+	std::size_t tmp_idx = hole_idx;
+	std::size_t child_idx = hole_idx * 2 + 1;
+	while (child_idx < _heap.size()) {
+		if (child_idx + 1 < _heap.size() && _heap[child_idx]->expire > _heap[child_idx+1]->expire) ++child_idx;
+		if (_heap[hole_idx]->expire < _heap[child_idx]->expire) break;
+		swap(_heap[hole_idx], _heap[child_idx]);
+		hole_idx = child_idx;
+		child_idx = hole_idx * 2 + 1;
+	}
+	return tmp_idx ^ hole_idx;
+}
+
 // 向堆中添加一个定时器
-template <typename Timer, typename Compare>
-void timer_heap<Timer, Compare>::push_timer(Timer *timer) {
+/* template <typename Timer, typename Compare> */
+template <typename Timer>
+void timer_heap<Timer>::push_timer(Timer *timer) {
 #ifndef NDEBUG
 	std::cout << "\nadd timer into timer heap..." << std::endl;
 #endif
-	// 需先使用push_back将timer添加到底层vector的尾部，再使用push_heap完成上溯
-	_heap->push_back(timer);
-	std::push_heap(_heap->begin(), _heap->end(), _compare);
+	// 新定时器的初始id为尾元素后一个索引位置
+	timer->id = _heap.size();
+	// 先使用push_back将timer添加到底层vector的尾部，再使用shift_up完成上滤
+	_heap.push_back(timer);
+	shift_up(timer->id);
+	/* std::push_heap(_heap->begin(), _heap->end(), _compare); */
 #ifndef NDEBUG
-	std::cout << "** (success) timer expire => " << get_format_time(timer->expire) << std::endl;
+	for (std::size_t i = 0; i < _heap.size(); ++i) {
+		std::cout << get_format_time(_heap[i]->expire) << " id: " << _heap[i]->id << std::endl;
+	}
 #endif
 }
 
 // 删除堆顶的定时器
-template <typename Timer, typename Compare>
-void timer_heap<Timer, Compare>::pop_timer() {
+template <typename Timer>
+void timer_heap<Timer>::pop_timer() {
 #ifndef NDEBUG
 	std::cout << "\npop timer from timer heap..." << std::endl;
 #endif
-	// 需先使用pop_heap将堆顶元素置于底层vector的尾部，并调整好堆结构
-	// 再通过底层vector的pop_back实现真正的删除（删除原先的堆顶元素）
-	std::pop_heap(_heap->begin(), _heap->end(), _compare);
+	// 先交换首尾元素，然后删除尾元素（原先的堆顶元素）
+	// 再对新的堆顶元素执行下滤操作
+	swap(_heap.front(), _heap.back());
+	delete _heap.back();
+	_heap.pop_back();
+	shift_down(0);
 #ifndef NDEBUG
-	std::cout << "** (success) timer expire => " << get_format_time(_heap->back()->expire) << std::endl;
+	for (std::size_t i = 0; i < _heap.size(); ++i) {
+		std::cout << get_format_time(_heap[i]->expire) << " id: " << _heap[i]->id << std::endl;
+	}
 #endif
-	_heap->pop_back();
+}
+
+// 删除堆中任意位置的定时器
+template <typename Timer>
+void timer_heap<Timer>::del_timer(Timer *timer) {
+#ifndef NDEBUG
+	std::cout << "\ndel timer from timer heap..." << std::endl;
+#endif
+	// 首先根据id快速定位定时器在堆中的位置
+	std::size_t hole_idx = timer->id;
+	// 将当前位置的元素与尾元素交换位置
+	swap(_heap[hole_idx], _heap.back());
+	// 删除尾元素（原先堆中任意位置的元素）
+	delete _heap.back();
+	_heap.pop_back();
+	// 如果上滤不成功就尝试下滤
+	if (!shift_up(hole_idx)) shift_down(hole_idx);
+#ifndef NDEBUG
+	for (std::size_t i = 0; i < _heap.size(); ++i) {
+		std::cout << get_format_time(_heap[i]->expire) << " id: " << _heap[i]->id << std::endl;
+	}
+#endif
 }
 
 // 心搏函数，处理到期的定时器
-template <typename Timer, typename Compare>
-void timer_heap<Timer, Compare>::tick() {
+template <typename Timer>
+void timer_heap<Timer>::tick() {
 	// 若堆中尚有元素，则通过循环来处理所有到期的定时器
-	while (!_heap->empty()) {
+	while (!_heap.empty()) {
 #ifndef NDEBUG
 		std::cout << "\ntick to process..." << std::endl;
 #endif
-		typename heap_type::iterator iter = _heap->begin();
+		typename heap_type::iterator iter = _heap.begin();
 		// 计算超时时间与当前时间间隔
 		expire_type now = std::chrono::high_resolution_clock::now();
 		interval_type interval_time = std::chrono::duration_cast<interval_type>((*iter)->expire - now);
